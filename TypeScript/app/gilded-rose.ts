bfd9adbf6f4c8a7aa3b529eb6ee9c6648b2c5cf2
Lucas Deschamps <lucas.deschamps@360learning.com>
2022-01-26 15:17:07 +0100

    remove default constructor value

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -13,7 +13,7 @@ export class Item {
 export class GildedRose {
   items: Array<Item>;
 
-  constructor(items = [] as Array<Item>) {
+  constructor(items: Array<Item>) {
     this.items = items;
   }
 
