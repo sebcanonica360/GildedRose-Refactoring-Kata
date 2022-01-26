1f4973e3fe828ea93a9f749d781fed1f8cd23640
Lucas Deschamps <lucas.deschamps@360learning.com>
2022-01-26 15:47:19 +0100

    extract updateExpiredItemQuality

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -57,17 +57,21 @@ export class GildedRose {
     }
 
     if (item.sellIn < 0) {
-      if (item.name == 'Aged Brie') {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
-      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        item.quality = item.quality - item.quality
-      } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
-      } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
+      this.updateExpiredItemQuality(item);
+    }
+  }
+
+  private static updateExpiredItemQuality(item: Item) {
+    if (item.name == 'Aged Brie') {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+      }
+    } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      item.quality = item.quality - item.quality
+    } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
+    } else {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
       }
     }
   }
