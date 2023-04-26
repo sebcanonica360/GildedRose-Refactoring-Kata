a588d071608f4d0fb0034031094977403ea38c31
Thierry Lalande <thierry.lalande@360learning.com>
2023-04-26 14:54:47 +0200

    tmp

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,7 +19,9 @@ export class GildedRose {
 
   updateQuality() {
     this.items.forEach((item: Item) => {
-      if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name === 'Aged Brie') {
+        this.increaseQuality(item);
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
         this.increaseQuality(item);
         if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (item.sellIn < 11) {
